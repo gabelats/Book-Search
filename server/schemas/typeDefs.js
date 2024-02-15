e0feacc898f0f49cd9ab98b78c8dc6e86beafe04
Gabreil Latimer <gabelats@gmail.com>
2024-02-14 17:01:54 -0700

    first commit

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const typeDefs = `
+    type User {
+        _id: ID
+        username: String
+        password: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String!
+        title: String!
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User!
+    }
+     input BookInput {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+    
+    
+    `;
+module.exports = typeDefs;
